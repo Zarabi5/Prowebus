abcf16a4d38abca9062ffa77571301ee84dfb8cf
Zarabi5 <zarabieye@gmail.com>
2023-09-01 11:13:04 -0500

    Mensaje de commit aquí

--- Formulario/index.php
+++ Formulario/index.php
@@ -118,6 +118,9 @@ formulario:
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
     crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
 </body>
 
 </html>
--- notas/connection/connection.php
+++ notas/connection/connection.php
@@ -1,17 +1,11 @@
 <?php
+$host = 'localhost';
+$user = 'nombre_de_usuario';
+$password = 'contraseña';
+$database = 'nombre_de_la_base_de_datos';
 
-// Datos de la conexión
-$host = "localhost";
-$user = "usuario";
-$password = "contraseña";
-$database = "nombre_de_la_base_de_datos";
-
-// Conexión a la base de datos
 $connection = mysqli_connect($host, $user, $password, $database);
 
-// Verificación de la conexión
 if (!$connection) {
-	die("Error de conexión: " . mysqli_connect_error());
+    die('Error al conectar a la base de datos: ' . mysqli_connect_error());
 }
-
-?>
